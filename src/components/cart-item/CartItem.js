316fd2bfb3d4136773cd39f19b055660da9631d1
hijoblimalika <nematxanovanaima@gmail.com>
2023-03-01 21:12:46 +0500

    oiuhyjgftrdeswadsfdghjhkjkl;l;kiukytrfesdcxc

--- src/components/cart-item/CartItem.js
+++ src/components/cart-item/CartItem.js
@@ -1,78 +1,105 @@
+
 import React, { useState } from 'react'
 import "./CartItem.css"
-import { ADD_TO_CART, REMOVE_CART } from '../../context/action/actionType'
+import { ADD_TO_CART, REMOVE_CART, } from '../../context/action/actionType'
 import { useDispatch, useSelector } from 'react-redux'
-function Cart({karzinka}) {
+import { Link } from 'react-router-dom'
+
+
+
+
+const BOT_TOKEN = "6010398140:AAEF5ETQmDxo9IM44R7wbNh05EvBlPUhsKc"
+const CHAT_ID = "-822999664"
+
+
+
+
+
+
+// 1. Chatni id sini olish uchun
+// https://api.telegram.org/bot6259790459:AAGg8ZTiFywTDaq8vUlzQxalz2b7AORj6FY/getUpdates
+
+
+// 2. Xabarni chatga yuborish uchun
+// https://api.telegram.org/bot[your_token]/sendMessage?chat_id=[your chat_id]&parse_mode=html
+
+function CartItem({ karzinka }) {
   const [name, setName] = useState("")
   const [tel, setTel] = useState("")
-  const [address, setAddress] = useState("")
+  const [adress, setAdress] = useState("")
   const [desc, setDesc] = useState("")
-  const dispatch = useDispatch()
-
-
-
-
+  const dispatch = useDispatch("")
+  const cart = useSelector(s => s.cart)
 
   const addToCart = (item) => {
-    let index = karzinka.findIndex(i=> i.id === item.id)
-    if( index < 0 ){
-      return dispatch({type: ADD_TO_CART, payload: [...karzinka, {...item, qty: 1}]})
+    let index = karzinka.findIndex(i => i.id === item.id)
+    if (index < 0) {
+      return dispatch({ type: ADD_TO_CART, payload: [...karzinka, { ...item, qty: 1 }] })
     }
-    let newCart = karzinka.map((pro, inx)=> inx === index ? {...pro, qty: pro.qty + 1} : pro)
-     
-    dispatch( {type: ADD_TO_CART, payload: newCart})
+    let newCart = karzinka.map((pro, inx) => inx === index ? { ...pro, qty: pro.qty + 1 } : pro)
+    dispatch({ type: ADD_TO_CART, payload: newCart })
   }
 
   const removeCart = (item) => {
-    let index = karzinka.findIndex(i=> i.id === item.id)
-
-    if( index < 0 ){
-      return dispatch({type: ADD_TO_CART, payload: [...karzinka, {...item, qty: 1}]})
+    let index = karzinka.findIndex(i => i.id === item.id)
+    if (index < 0) {
+      return dispatch({ type: ADD_TO_CART, payload: [...karzinka, { ...item, qty: 1 }] })
     }
-    let newCart = karzinka.map((pro, inx)=> inx === index ? {...pro, qty: pro.qty - 1} : pro)
-     
-    dispatch( {type: ADD_TO_CART, payload: newCart})
+    let newCart = karzinka.map((pro, inx) => inx === index ? { ...pro, qty: pro.qty - 1 } : pro)
+    dispatch({ type: ADD_TO_CART, payload: newCart })
   }
-  const cart = useSelector(s => s.cart)
-
-  const order = ()=>{
-    console.log( { name, tel, address, desc, cart } );
+  const order = () => {
+    let msg = ""
+    msg += `Ism:<b>${name}</b> %0A`// %0A = <br/>
+    msg += `Tel:${tel}%0A`
+    msg += `Address:${adress}%0A`
+    msg += `Ta'rif:${desc}%0A`
+    msg += `%0A<b>Buyurtmalar</b>%0A%0A`
+    karzinka.forEach((order) => {
+      msg += `Nomi: ${order.title}%0A`
+      msg += `Soni: ${order.qty}%0A`
+      msg += `Narxi: ${order.price}%0A`
+    })
+    const API_URL = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage?chat_id=${CHAT_ID}&text=${msg}&parse_mode=html`
+    let api = new XMLHttpRequest();
+    api.open("GET", API_URL, true);
+    api.send();
+    const deletInput = () => {
+      setName("")
+      setTel("")
+      setAdress("")
+      setDesc("")
+    }
+    deletInput()
   }
-
   
   return (
-    <div className='cartt'>
-        {/* <h2>Jami narxi: {karzinka.reduce((a, b) => a + (b.price * b.qty), 0).brm()} so'm</h2> */}
-        <input value={name} onChange={e => setName(e.target.value)} type="text" placeholder='Ism' />
-        <br />
-        <input value={tel} onChange={e => setTel(e.target.value)} type="text" placeholder='tel' />
-        <br />
-        <input value={address} onChange={e => setAddress(e.target.value)} type="text" placeholder='address' />
-        <br />
-        <textarea value={desc} onChange={e => setDesc(e.target.value)} name="" placeholder='Qanday tovar kerakligi razmer...' id="" cols="30" rows="10"></textarea>
-        <br />
-        <button onClick={order}>Buyurtma berish</button>
-        <div className='carts'>
-          {
-            karzinka.map((item, inx)=> <div key={inx} className="cart__item">
-              <div className="cart__img">
-                <img src={item?.urls[0]} width={100} alt="" />
-              </div>
-              <div className="cart__text">
-                <p className='cart__name'>{item?.title}</p>
-                {/* <p className='cart__price'>{item?.price.brm()} so'm</p> */}
-              </div>
-              <div className="cart__button">
-                <button className='cart__minus' disabled={ item?.qty <= 1 } onClick={()=> removeCart(item)}>-</button>
-                <span className='cart__number'>{item?.qty}</span>
-                <button className='cart__plus' onClick={()=> addToCart(item)}>+</button>
-                <button className='cart__delete' onClick={()=> dispatch({type: REMOVE_CART, payload: item.id})}>delete</button>
-              </div>
-            </div>)
-          }
+    <div>
+      <div style={{ textAlign: "center" }}>
+        <div className="inputs">
+          <input className='cart__input' value={name} onChange={e => setName(e.target.value)} type="text" placeholder='name' />
+          <input className='cart__input' value={tel} onChange={e => setTel(e.target.value)} type="number" placeholder='tel' />
+          <input className='cart__input' value={adress} onChange={e => setAdress(e.target.value)} type="text" placeholder='address' />
+          <textarea className='cart__texta' value={desc} onChange={e => setDesc(e.target.value)} name="" placeholder='desc' id="" cols="30" rows="10"></textarea>
+          <button onClick={order}>Buyurtma berish</button>
         </div>
+      </div>
+      <div className=" cartItem">
+        {
+          karzinka?.map((item, inx) => <div key={inx} className="cart__item">
+            <img src={item?.urls[0]} width={100} alt="" />
+            <Link to={`/product/${item.id}`}><p>{item?.title}</p></Link>
+            <div className="qty">
+              <button className='minus' disabled={item?.qty <= 1} onClick={() => removeCart(item)}>-</button>
+              <span>{item?.qty}</span>
+              <button className='plas' onClick={() => addToCart(item)}>+</button>
+            </div>
+            <button className='delet' onClick={() => dispatch({ type: REMOVE_CART, payload: item.id })} >delete</button>
+          </div>)
+        }
+      </div>
     </div>
   )
 }
 
-export default Cart
+export default CartItem
--- src/server/index.js
+++ src/server/index.js
@@ -4,13 +4,13 @@ import { getFirestore } from "@firebase/firestore"
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDkx93VLk72Y00PGni8vCYJ_9m85trTcm0",
-    authDomain: "alpha-d50a4.firebaseapp.com",
-    projectId: "alpha-d50a4",
-    storageBucket: "alpha-d50a4.appspot.com",
-    messagingSenderId: "702939705396",
-    appId: "1:702939705396:web:a852ca44daec37b5ffda61",
-    measurementId: "G-923GGB9Z69"
+  apiKey: "AIzaSyBnxkhoUR7kWHGcsdMXDDZHB-p2TrtABEc",
+  authDomain: "new-brand-e587b.firebaseapp.com",
+  projectId: "new-brand-e587b",
+  storageBucket: "new-brand-e587b.appspot.com",
+  messagingSenderId: "452487507086",
+  appId: "1:452487507086:web:3c744d372f3e0866206ddf",
+  measurementId: "G-7LEYH4H5ZM"
 };
 
 const app = initializeApp(firebaseConfig)
